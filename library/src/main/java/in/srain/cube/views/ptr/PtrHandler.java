74bc1ffda66c4b293e4d414ab1392c2e093d11cf
Soulwolf Ching <ching.soulwolf@gmail.com>
2017-06-12 16:23:00 +0800

    fix Project structure

---
+++ library/src/main/java/in/srain/cube/views/ptr/PtrHandler.java
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (C) 2015 Leholady(乐活女人) Inc. All rights reserved.
+ */
+
+package in.srain.cube.views.ptr;
+
+import android.view.View;
+
+public interface PtrHandler {
+
+    /**
+     * Check can do refresh or not. For example the content is empty or the first child is in view.
+     * <p/>
+     * {@link PtrDefaultHandler#checkContentCanBePulledDown}
+     */
+    public boolean checkCanDoRefresh(final PtrFrameLayout frame, final View content, final View header);
+
+    /**
+     * When refresh begin
+     *
+     * @param frame
+     */
+    public void onRefreshBegin(final PtrFrameLayout frame);
+}
